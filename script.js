f76de4f447351725424aab7f3e204e2035c646ca
SpragueWandsworth <thetelescope46@gmail.com>
2026-01-22 14:16:28 +0000

    basic setup code

---
+++ script.js
@@ -0,0 +1,9 @@
+//speed be player size?
+
+const canvas = document.getElementById("game-window");
+const ctx = canvas.getContext("2d");
+
+const gameWidth = canvas.width;
+const gameHeight = canvas.height;
+
+ctx.fillText("chumley", gameWidth/2, gameHeight/2);
