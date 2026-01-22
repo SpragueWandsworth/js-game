e51e3845f2c9f3c4bf076e056a9499d46eb3ff03
SpragueWandsworth <thetelescope46@gmail.com>
2026-01-22 14:38:40 +0000

    added visible player

--- script.js
+++ script.js
@@ -6,4 +6,20 @@ const ctx = canvas.getContext("2d");
 const gameWidth = canvas.width;
 const gameHeight = canvas.height;
 
-ctx.fillText("chumley", gameWidth/2, gameHeight/2);
+const playerColor = "Green";
+const playerSize = 25;
+
+let player = {x: 20, y: 20};
+
+//ctx.fillText("chumley", gameWidth/2, gameHeight/2);
+
+startGame();
+
+function drawPlayer() {
+    ctx.fillStyle = playerColor;
+    ctx.fillRect(player.x, player.y, playerSize, playerSize);
+};
+
+function startGame() {
+    drawPlayer();
+};
